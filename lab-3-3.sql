123dac37ae46ff8b8ce67fc1c8d6f3c7cb281af7
Tim Gray <tim.gray@kellogg.northwestern.edu>
2024-01-11 03:22:53 +0000

    section 4 labs

--- lab-3-3.sql
+++ lab-3-3.sql
@@ -1,7 +1,7 @@
 -- In the modern era (1960-present), how many regular season games did the best team win each season?
 -- Challenge: Try to include the team's name in the results. Is it accurate?
 
-SELECT year, name, MAX(wins)
+SELECT year, MAX(wins)
 FROM teams
 WHERE year > 1959
 GROUP BY year;
--- lab-4-2.sql
+++ lab-4-2.sql
@@ -1,6 +1,11 @@
 -- What are the first and last names of the players who 
 -- played for the 2020 Chicago Cubs?
 
+SELECT players.first_name, players.last_name 
+FROM players INNER JOIN stats ON players.id = stats.player_id
+LIMIT 3; 
+
+
 -- Hint: combine WHERE clauses using AND, e.g.
 -- WHERE column1 = expression1
 -- AND column2 = expression2
